27d3e9f2a202d7d57dd8f6d45bc67137156e6b2f
Chiara Mannari <chiara@Mac.homenet.telecomitalia.it>
2025-11-08 12:34:16 +0100

    added logger in file in render

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function createLogger() {
   const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
   const filename = `blockly_session_${timestamp}.log`;
 
-    const logtail = new Logtail($process.env.LOGTAIL_SOURCE_TOKEN, {
+    const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN, {
     // Optional: use default endpoint unless Logtail gives you a custom one
     endpoint: "https://s1582689.eu-nbg-2.betterstackdata.com", 
   });
