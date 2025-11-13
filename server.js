58c08be1722a6eb5b832ce2d35b75c2886be9433
Chiara Mannari <chiara@Mac.homenet.telecomitalia.it>
2025-11-13 11:36:01 +0100

    api gemini model process.env fix

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post("/ask-ai", async (req, res) => {
     });*/
     //const result = await model.generateContent(userMessage);
     const result = await genAIai.models.generateContent({
-    model: process.env.GEMINI_MODEL || "gemini-1.5-flash",
+      model: "gemini-1.0-pro",  
     contents: userMessage,
   });
   console.log(response.text);
